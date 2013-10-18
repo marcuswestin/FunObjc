732dfbcb7fa682563500d39cfeb0bb84c6ecdb3c
Marcus Westin <marcus.westin@gmail.com>
2013-10-17 18:42:51 -0700

    log completed migrations

--- More/SQL/SQL.m
+++ More/SQL/SQL.m
@@ -69,6 +69,7 @@ static NSMutableDictionary* columnsCache;
                 rollback();
             }
         }];
+        NSLog(@"Completed migration %@", migration[@"name"]);
         [_completedMigrations addObject:migration[@"name"]];
     }];
     
