0d5250768c4c884a55a03d2c9ad7b7cdd895698a
Marcus Westin <marcus.westin@gmail.com>
2013-10-17 18:41:18 -0700

    Add NSNull category

---
+++ Categories/NSNull+Fun.h
@@ -0,0 +1,12 @@
+//
+//  NSNull+Fun.h
+//  Dogo iOS
+//
+//  Created by Marcus Westin on 10/17/13.
+//  Copyright (c) 2013 Flutterby Labs Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNull (Fun)
+@end
---
+++ Categories/NSNull+Fun.m
@@ -0,0 +1,13 @@
+//
+//  NSNull+Fun.m
+//  Dogo iOS
+//
+//  Created by Marcus Westin on 10/17/13.
+//  Copyright (c) 2013 Flutterby Labs Inc. All rights reserved.
+//
+
+#import "NSNull+Fun.h"
+
+@implementation NSNull (Fun)
+
+@end
