6cbd576f7d178a31fa8c7dce95b3119a4c1a6d92
Marcus Westin <narcvs@gmail.com>
2013-11-11 15:23:29 -0800

    Move More/* stuff with no external dependencies to better places

---
+++ More/FunMore.h
@@ -0,0 +1,14 @@
+//
+//  FunBase.h
+//  Dogo-iOS
+//
+//  Created by Marcus Westin on 6/25/13.
+//  Copyright (c) 2013 Flutterby Labs Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "FunTypes.h"
+#import "FunCategories.h"
+#import "FunUseful.h"
+#import "FunData.h"
