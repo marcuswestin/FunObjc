07230e744afb1735e473ed1e18119b1eb85add70
Marcus Westin <narcvs@gmail.com>
2013-11-12 18:11:11 -0800

    better num suffix

--- Categories/NSNumber+Fun.m
+++ Categories/NSNumber+Fun.m
@@ -19,14 +19,23 @@
 }
 
 + (NSString *)suffix:(NSInteger)d {
+    d = d % 100;
+    if (d >= 11 && d <= 13) {
+        // 11th, 12th, 13th, 111th, 2013th, ...
+        return @"th";
+    }
     d = d % 10;
     if (d == 1) {
+        // 1st, 21st, 101st, 4301st, ...
         return @"st";
     } else if (d == 2) {
+        // 2nd, ...
         return @"nd";
     } else if (d == 3) {
+        // 3rd, ...
         return @"rd";
     } else {
+        // 0th, 4th, 10th, 14th, 20th, 100th, ...
         return @"th";
     }
 }
