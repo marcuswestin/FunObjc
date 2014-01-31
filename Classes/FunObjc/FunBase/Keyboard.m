b58868c16f5853145fab3c29fa9267e6bd8e0050
Marcus Westin <marcus.westin@gmail.com>
2014-01-31 10:29:23 -0800

    Add Keyboard willChange, with appropriate height change even in the case of willHide directly followed by willShow

--- Classes/FunObjc/FunBase/Keyboard.m
+++ Classes/FunObjc/FunBase/Keyboard.m
@@ -8,6 +8,7 @@
 
 #import "Keyboard.h"
 #import "Viewport.h"
+#import "FunGlobals.h"
 
 @implementation KeyboardEventInfo
 - (void)animate:(void (^)(void))animations {
@@ -24,6 +25,7 @@ static Keyboard* instance;
     NSNotificationCenter* notifications = [NSNotificationCenter defaultCenter];
     [notifications addObserver:instance selector:@selector(_keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
     [notifications addObserver:instance selector:@selector(_keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    [notifications addObserver:instance selector:@selector(_keyboardWillChangeFrame:) name:UIKeyboardWillChangeFrameNotification object:nil];
 }
 
 + (void)onWillShow:(EventSubscriber)subscriber callback:(KeyboardEventCallback)callback {
@@ -34,12 +36,20 @@ static Keyboard* instance;
     [Events on:@"KeyboardWillHide" subscriber:subscriber callback:callback];
 }
 
++ (void)onWillChange:(EventSubscriber)subscriber callback:(KeyboardEventCallback)callback {
+    [Events on:@"KeyboardWillChange" subscriber:subscriber callback:callback];
+}
+
 + (void)offWillShow:(EventSubscriber)subscriber {
     [Events off:@"KeyboardWillShow" subscriber:subscriber];
 }
 
 + (void)offWillHide:(EventSubscriber)subscriber {
     [Events off:@"KeyboardWillHide" subscriber:subscriber];
+}
+
++ (void)offWillChange:(EventSubscriber)subscriber {
+    [Events off:@"KeyboardWillChange" subscriber:subscriber];
 }
 
 + (void)dismiss {
@@ -75,16 +85,46 @@ static Keyboard* instance;
 }
 
 - (void)_keyboardWillShow:(NSNotification*)notification {
+    KeyboardEventInfo* info = [self _keyboardInfo:notification isShowing:YES];
     instance.isVisible = YES;
-    instance.visibleHeight = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
-    [Events syncFire:@"KeyboardWillShow" info:[self _keyboardInfo:notification isShowing:YES]];
+    instance.visibleHeight = info.height;
+    [Events syncFire:@"KeyboardWillShow" info:info];
 }
 
 - (void)_keyboardWillHide:(NSNotification*)notification {
+    KeyboardEventInfo* info = [self _keyboardInfo:notification isShowing:NO];
     instance.isVisible = NO;
-    instance.visibleHeight = 0;
-    [Events syncFire:@"KeyboardWillHide" info:[self _keyboardInfo:notification isShowing:NO]];
+    instance.visibleHeight = info.height;
+    [Events syncFire:@"KeyboardWillHide" info:info];
 }
+
+- (void)_keyboardWillChangeFrame:(NSNotification*)notification {
+    [self _scheduleFireChange:notification];
+    
+}
+
+static NSNotification* nextNotification;
+- (void)_scheduleFireChange:(NSNotification*)notification {
+    @synchronized(nextNotification) {
+        if (nextNotification) {
+            nextNotification = notification;
+            return;
+        }
+    }
+    // A visible keyboard issues a "willHide" then a "willShow" (along with two "willChange")
+    // "willHide" reports a -216 height change (because it would change by -216).
+    // "willShow" reports a 0 height change (because the keyboard is already visible).
+    // So we wait a brief moment. In case of two "willChange", we report only the second (with 0 height change).
+    async(^{
+        KeyboardEventInfo* info;
+        @synchronized(nextNotification) {
+            info = [self _keyboardInfo:nextNotification isShowing:instance.isVisible];
+            nextNotification = nil;
+        }
+        [Events syncFire:@"KeyboardWillChange" info:info];
+    });
+}
+
 
 - (KeyboardEventInfo*)_keyboardInfo:(NSNotification*)notif isShowing:(BOOL)isShowing {
     KeyboardEventInfo* info = [KeyboardEventInfo new];
